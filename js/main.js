ddaaf949e5cf14d2d3bb14070633e5766b10164a
Luis Carrera <luiscarreradesndev@gmail.com>
2024-04-28 16:08:53 -0500

    Fixing issues with animation on some interacting components. Split components and tabs scripts file.

--- js/main.js
+++ js/main.js
@@ -4,20 +4,3 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
-
-// TODO Refractor code.
-if (screen.width < 1020) document.getElementById("defaultOpen").click();
-
-function openTab(evt, tabName) {
-  let i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
